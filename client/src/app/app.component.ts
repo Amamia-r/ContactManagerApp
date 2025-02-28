ed587963fbcf5a19c7d51684879fb2aa592005a0
Mamiantsa Rasoanaivo <mamiantsa404@gmail.com>
2025-02-28 14:22:33 +0300

    End of section 2

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'Contact Manager';
+  contacts: any;
+  
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/contact').subscribe(
+      {
+        next: response => this.contacts = response,
+        error: error => console.log(error),
+        complete: () => console.log('Request has completed')
+      }
+    )
+  }
+
+
+}
